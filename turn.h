2843c523c5a93f7351518786d124c716c07b1539
SirNiklaous <63741951+SirNiklaous@users.noreply.github.com>
2021-08-05 12:20:23 -0500

    Delete turn.h

--- turn.h
+++
@@ -1,9 +0,0 @@
-namespace vex {
-  class turn {
-    public:
-      static void turn1(float d, float s);
-      static void turn2(float d, float s);
-      static void turn3(double degrees);
-      static void turn4(double degrees);
-  };
-}
